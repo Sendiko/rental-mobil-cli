b559d36674352fc2d8e5a680ba924a665b42f419
Sendiko <sarangtawon897@gmail.com>
2025-06-25 20:15:15 +0700

    Melengkapi class Car

--- Car.java
+++ Car.java
@@ -1,16 +1,39 @@
 
 class Car {
 
-    String name;
-    String licensePlate;
-    String brand;
-    String model;
-    int year;
-    double pricePerDay;
-    boolean isAvailable;
+    /**
+     * Access modifier
+     * 
+     * Cara untuk menentukan akses untuk Atribut dan Method.
+     */
+    private String name;
+    private String licensePlate;
+    private String brand;
+    private String model;
+    private int year;
+    private double pricePerDay;
+    private boolean isAvailable;
 
-    public Car(String name, String licensePlate, String brand, String model, int year, double pricePerDay,
-            boolean isAvailable) {
+    /**
+     * Constructor untuk membuat object `Car`
+     * @param name
+     * @param licensePlate
+     * @param brand
+     * @param model
+     * @param year
+     * @param pricePerDay
+     * @param isAvailable
+     */
+
+    public Car(
+        String name,
+        String licensePlate,
+        String brand,
+        String model,
+        int year,
+        double pricePerDay,
+        boolean isAvailable
+    ) {
         this.name = name;
         this.licensePlate = licensePlate;
         this.brand = brand;
@@ -20,47 +43,79 @@ class Car {
         this.isAvailable = isAvailable;
     }
 
+    /**
+     * 
+     * Getter dan Setter
+     * 
+     * Fungsi untuk membantu mengakses Atribut.
+     * 
+     */
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getLicensePlate() {
         return licensePlate;
     }
 
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
     public String getBrand() {
         return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 
     public String getModel() {
         return model;
     }
 
+    public void setModel(String model) {
+        this.model = model;
+    }
+
     public int getYear() {
         return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     public double getPricePerDay() {
         return pricePerDay;
     }
 
-    public boolean isAvailable() {
+    public void setPricePerDay(double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public boolean getIsAvailable() {
         return isAvailable;
     }
 
-    public void setAvailable(boolean isAvailable) {
+    public void setIsAvailable(boolean isAvailable) {
         this.isAvailable = isAvailable;
     }
 
     @Override
     public String toString() {
-        return "====> Detail mobil <====" + 
-            "\nNama Mobil: " + name + 
-            "\nPlat Nomer: " + licensePlate + 
-            "\nBrand: " + brand + 
-            "\nModel: " + model + 
-            "\nTahun: " + year + 
-            "\nHarga/Hari: " + pricePerDay + 
-            "\nAvailable: " + isAvailable;
+        return "==> Detail Mobil <==" + 
+        "\nNama: " + name + 
+        "\nBrand: " + brand + 
+        "\nModel: " + model + 
+        "\nYear: " + year + 
+        "\nHarga/Hari: Rp." + pricePerDay + 
+        "\nTersedia: " + isAvailable;
     }
+
 }
